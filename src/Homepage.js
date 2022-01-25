00344528d1119bd548e36e77df8eafac1ff05362
Tomd304 <tomd304@live.co.uk>
2022-01-25 16:54:18 +0000

    Albums added and displayed on shop page

--- src/Homepage.js
+++ src/Homepage.js
@@ -11,3 +11,48 @@ const Homepage = () => {
 };
 
 export default Homepage;
+
+const vinylRecords = [
+  {
+    id: 1,
+    artistName: "The XX",
+    albumName: "See You",
+    price: "£15.90",
+    img: "https://i4.cdn.hhv.de/catalog/475x475/00507/507234.jpg",
+  },
+  {
+    id: 2,
+    artistName: "Nas",
+    albumName: "King's Disease II",
+    price: "£25.11",
+    img: "https://i1.cdn.hhv.de/catalog/475x475/00848/848096_2.jpg",
+  },
+  {
+    id: 3,
+    artistName: "Oddisee",
+    albumName: "The Odd Tape",
+    price: "£22.59",
+    img: "https://i2.cdn.hhv.de/catalog/shop_detail_zoom/00863/863392_1.jpg",
+  },
+  {
+    id: 4,
+    artistName: "Madlib",
+    albumName: "Sound Ancestors",
+    price: "£27.62",
+    img: "https://i2.cdn.hhv.de/catalog/shop_detail_zoom/00882/882797.jpg",
+  },
+  {
+    id: 5,
+    artistName: "Knxwledge",
+    albumName: "So Nice",
+    price: "£20.92",
+    img: "https://i4.cdn.hhv.de/catalog/shop_detail_zoom/00791/791134_1.jpg",
+  },
+  {
+    id: 6,
+    artistName: "Apollo Brown",
+    albumName: "Clouds",
+    price: "£19.25",
+    img: "https://i0.cdn.hhv.de/catalog/shop_detail_zoom/00846/846490_1.jpg",
+  },
+];
--- src/Shop.js
+++ src/Shop.js
@@ -1,11 +1,25 @@
 import Navbar from "./components/Navbar";
 import index from "./index.css";
+import getRecords from "./records";
+import { useState } from "react";
 
 const Shop = () => {
+  const [records, setRecords] = useState(getRecords());
+
   return (
     <div>
       <Navbar page="shop" />
       <h1>This is the shop component</h1>
+      {records.map((record) => (
+        <div className="card">
+          <img
+            style={{ height: 200 }}
+            src={record.img}
+            alt={record.artistName + record.albumName}
+          />
+          <p>{record.artistName + " " + record.albumName}</p>
+        </div>
+      ))}
     </div>
   );
 };
