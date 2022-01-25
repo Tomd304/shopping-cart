18bab678f55e1308f7497171270bc8b622ae4b8a
Tomd304 <tomd304@live.co.uk>
2022-01-25 15:30:52 +0000

    Initial commit

---
+++ src/Homepage.js
@@ -0,0 +1,5 @@
+const Homepage = () => {
+  return <h1>This is the homepage component</h1>;
+};
+
+export default Homepage;
---
+++ src/index.js
@@ -0,0 +1,10 @@
+import React from "react";
+import ReactDOM from "react-dom";
+import Switcher from "./Switcher";
+
+ReactDOM.render(
+  <React.StrictMode>
+    <Switcher />
+  </React.StrictMode>,
+  document.getElementById("root")
+);
