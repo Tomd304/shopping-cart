68f98207ac6f03a04928b31157d930573030cb27
Tomd304 <tomd304@live.co.uk>
2022-01-26 15:35:37 +0000

    Shop card styling updated

--- src/Shop.js
+++ src/Shop.js
@@ -1,5 +1,5 @@
 import Navbar from "./components/Navbar";
-import index from "./index.css";
+import shopStyle from "./shop.css";
 import getRecords from "./records";
 import { useState } from "react";
 
@@ -10,16 +10,22 @@ const Shop = () => {
     <div>
       <Navbar page="shop" />
       <h1>This is the shop component</h1>
-      {records.map((record) => (
-        <div className="card">
-          <img
-            style={{ height: 200 }}
-            src={record.img}
-            alt={record.artistName + record.albumName}
-          />
-          <p>{record.artistName + " " + record.albumName}</p>
-        </div>
-      ))}
+      <div className="cardContainer">
+        {records.map((record) => (
+          <div className="card">
+            <img
+              style={{ width: 300 }}
+              src={record.img}
+              alt={record.artistName + record.albumName}
+            />
+            <div className="cardText">
+              <p className="cardArtist">{record.artistName}</p>
+              <p className="cardAlbum">{record.albumName}</p>
+              <p className="cardPrice">LP | {record.price}</p>{" "}
+            </div>
+          </div>
+        ))}
+      </div>
     </div>
   );
 };
