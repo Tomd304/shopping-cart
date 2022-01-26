2f9d85bc598ba856bce4ffbf7d9d3a9808fd4223
Tomd304 <tomd304@live.co.uk>
2022-01-26 15:43:25 +0000

    Additional records added to display

--- src/records.js
+++ src/records.js
@@ -42,6 +42,34 @@ const getRecords = () => {
       price: 19.25,
       img: "https://i0.cdn.hhv.de/catalog/shop_detail_zoom/00846/846490_1.jpg",
     },
+    {
+      id: 7,
+      artistName: "KRS-One",
+      albumName: "Sound Of Da Police",
+      price: 17.96,
+      img: "https://i1.cdn.hhv.de/catalog/475x475/00856/856801_1.jpg",
+    },
+    {
+      id: 8,
+      artistName: "Royce Da 5'9",
+      albumName: "The Allegory",
+      price: 21.69,
+      img: "https://i3.cdn.hhv.de/catalog/shop_detail_zoom/00851/851328_7.jpg",
+    },
+    {
+      id: 9,
+      artistName: "Czarface & MF DOOM",
+      albumName: "Czarface Meets Metal Face",
+      price: 21.58,
+      img: "https://i0.cdn.hhv.de/catalog/shop_detail_zoom/00866/866395.jpg",
+    },
+    {
+      id: 10,
+      artistName: "Notorious B.I.G",
+      albumName: "Ready To Die",
+      price: 25.86,
+      img: "https://i3.cdn.hhv.de/catalog/475x475/00847/847753_5.jpg",
+    },
   ];
 };
 
